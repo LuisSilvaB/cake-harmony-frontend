11892f246497176db9bd7e541b89d9bbaf46e34d
LuisSilvaB <luisarmandoballadares@gmail.com>
2024-08-10 04:37:55 -0500

    chore: add navbar layout

---
+++ src/Components/layout/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full h-10 shadow-md flex flex-col items-center justify-center bg-white'>
+      <div className='w-full h-full flex items-center max-w-screen-2xl px-2 sm:px-6 lg:px-8 border'>
+        <p>CH</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
